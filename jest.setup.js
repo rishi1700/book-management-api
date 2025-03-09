f4b6ac080947e79b4383f704c712d7c403d36164
rishi1700 <prasadrishi170@gmail.com>
2025-03-09 23:24:43 +0000

    Fix: Remove ignored files from Git tracking

--- jest.setup.js
+++
@@ -1,8 +0,0 @@
-// Suppress unnecessary console logs during tests
-global.console = {
-    log: jest.fn(), // Hide console.log
-    info: jest.fn(), // Hide console.info
-    warn: console.warn, // Keep console.warn visible
-    error: console.error, // Keep console.error visible for debugging
-  };
-  
